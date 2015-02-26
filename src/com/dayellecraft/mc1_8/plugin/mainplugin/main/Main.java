cd3216a886abeae07c43c94a1024fc69b14e3e0c
CyanFrost <patricknopely@gmail.com>
2015-02-26 08:04:13 -0600

    Add Main Class
    Added Main.java to the workspace.

---
+++ src/com/dayellecraft/mc1_8/plugin/mainplugin/main/Main.java
@@ -0,0 +1,23 @@
+package com.dayellecraft.mc1_8.plugin.mainplugin.main;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin{
+
+	public static void main(String[] args) {
+
+	}
+
+	@Override public void onLoad(){
+
+	}
+
+	@Override public void onEnable(){
+
+	}
+
+	@Override public void onDisable(){
+
+	}
+
+}
